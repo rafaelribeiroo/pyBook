d23ab83b9899c4f809720ac6b00cd306d96f9b19
Rafael1994 <pereiraribeirorafael@gmail.com>
2017-07-14 14:59:46 -0300

    Ultimas alterações

--- src/books/views.py
+++ src/books/views.py
@@ -1,21 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import JsonResponse
 from django.template.loader import render_to_string
-from .forms import LivroForm
 
-# Create your views here.
 from .models import Livro
+from .forms import LivroForm
 
 def livro(request):
     livros = Livro.objects.all()
     return render(request, 'livros.html', {'livros': livros})
 
-def criarLivro(request):
-    form = LivroForm()
+def salvar_form_livro(request, form, template_name):
+    data = dict()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            livros = Livro.objects.all()
+            data['html_book_list'] = render_to_string('listaLivros.html', {
+                'livros': livros
+            })
+        else:
+            data['form_is_valid'] = False
     context = {'form': form}
-    html_form = render_to_string('cadastraLivro.html',
-        context,
-        request=request,
-    )
-    return JsonResponse({'html_form': html_form})
+    data['html_form'] = render_to_string(template_name, context, request=request)
+    return JsonResponse(data)
+
+def criarLivro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+    else:
+        form = LivroForm()
+    return salvar_form_livro(request, form, 'cadastraLivro.html')
+
+def alteraLivro(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == 'POST':
+        form = LivroForm(request.POST, instance=livro)
+    else:
+        form = LivroForm(instance=livro)
+    return salvar_form_livro(request, form, 'alteraLivro.html')
+
+def deletaLivro(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    data = dict()
+    if request.method == 'POST':
+        livro.delete()
+        data['form_is_valid'] = True  # This is just to play along with the existing code
+        livros = Livro.objects.all()
+        data['html_book_list'] = render_to_string('listaLivros.html', {
+            'livros': livros
+        })
+    else:
+        context = {'livro': livro}
+        data['html_form'] = render_to_string('deletaLivro.html',
+            context,
+            request=request,
+        )
+    return JsonResponse(data)
--- src/core/static/js/livros.js
+++ src/core/static/js/livros.js
@@ -1,8 +1,11 @@
 $(function () {
 
-    $(".js-create-book").click(function (){
+    /* Functions */
+
+    var loadForm = function () {
+        var btn = $(this);
         $.ajax({
-            url: '/livros/criar',
+            url: btn.attr("data-url"),
             type: 'get',
             dataType: 'json',
             beforeSend: function () {
@@ -11,7 +14,42 @@ $(function () {
             success: function (data) {
                 $("#modal-book .modal-content").html(data.html_form);
             }
-        })
-    })
+        });
+    };
+
+    var saveForm = function () {
+        var form = $(this);
+        $.ajax({
+            url: form.attr("action"),
+            data: form.serialize(),
+            type: form.attr("method"),
+            dataType: 'json',
+            success: function (data) {
+                if (data.form_is_valid) {
+                    $("#book-table tbody").html(data.html_book_list);
+                    $("#modal-book").modal("hide");
+                }
+                else {
+                    $("#modal-book .modal-content").html(data.html_form);
+                }
+            }
+        });
+        return false;
+    };
+
+
+    /* Binding */
+
+    // Create book
+    $(".js-create-book").click(loadForm);
+    $("#modal-book").on("submit", ".js-book-create-form", saveForm);
+
+    // Update book
+    $("#book-table").on("click", ".js-update-book", loadForm);
+    $("#modal-book").on("submit", ".js-book-update-form", saveForm);
+
+    // Delete book
+    $("#book-table").on("click", ".js-delete-book", loadForm);
+    $("#modal-book").on("submit", ".js-book-delete-form", saveForm);
 
 });
