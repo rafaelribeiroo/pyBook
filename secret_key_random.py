59a6eb04c58a4932ccab723421d33a9f7ea8a8e6
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-06-13 15:34:42 -0300

    Erros sendo corrigidos

---
+++ secret_key_random.py
@@ -0,0 +1,3 @@
+import random
+import string
+print(''.join([random.SystemRandom().choice("{}{}{}".format(string.ascii_letters, string.digits, string.punctuation)) for i in range(50)]))
--- src/books/urls.py
+++ src/books/urls.py
@@ -1,16 +1,15 @@
 from django.conf.urls import url
-from django.contrib import admin
 
 from .views import (
     livro,
     criarLivro,
     alteraLivro,
     deletaLivro
-    )
+)
 
 urlpatterns = [
     url(r'^$', livro, name='livro_lista'),
-    url(r'^criar/$', criarLivro, name="livro_criar"),
+    url(r'^criar/$', criarLivro, name='livro_criar'),
     url(r'^(?P<pk>\d+)/alterar/$', alteraLivro, name='livro_alterar'),
     url(r'^(?P<pk>\d+)/deletar/$', deletaLivro, name='livro_deletar'),
 ]
